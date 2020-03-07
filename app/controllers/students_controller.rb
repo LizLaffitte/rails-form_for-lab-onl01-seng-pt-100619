4b372a696e5660cc24ea0138e879aa0623f8010a
Liz Laffitte <liz.kossakowski@gmail.com>
2020-03-07 20:53:17 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,20 @@
 class StudentsController < ApplicationController
+    def new 
+    @school_class = SchoolClass.new
+  end
+  
+  def create
+    @school_class = SchoolClass.new(school_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+  
+  def show 
+  end
+  
+  private 
+    def school_params
+      params.require(:school_class).permit(:title, :room_number)
+    end
+  
 end
